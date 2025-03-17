1e0ab83f3264b8efc06283bcdd6bae68a530215f
Thanh21062006 <Caotatthanh2006@gmail.com>
2025-03-17 21:38:49 +0700

    Session16Session16

---
+++ Session16/BT2/main2.js
@@ -0,0 +1,11 @@
+let number = 10;
+document.writeln(number + "\n");
+
+let price = 20.5;
+document.writeln(price + "\n");
+
+let str = 'Hello World';
+document.writeln(str.includes('Hello') + "\n");
+
+let city = "Hà Nội";
+document.writeln(city + "\n");
